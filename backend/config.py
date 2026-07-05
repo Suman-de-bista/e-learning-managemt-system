b61a295b04e129ffc02d7794f3675b6b6f4980c3
Suman Bista <61659676+Suman-de-bista@users.noreply.github.com>
2026-07-05 08:28:54 +0545

    Chore: Config

--- backend/config.py
+++ backend/config.py
@@ -13,3 +13,11 @@ DATABASE_URL: str = getenv(
 DATABASE_MIN_SIZE: int = int(getenv("DATABASE_MIN_SIZE", "2"))
 DATABASE_MAX_SIZE: int = int(getenv("DATABASE_MAX_SIZE", "5"))
 
+SECRET_KEY: str = getenv("SECRET_KEY", "")
+ALGORITHM: str = getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES: int = int(getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+REFRESH_TOKEN_EXPIRE_DAYS: int = int(getenv("REFRESH_TOKEN_EXPIRE_DAYS", "7"))
+
+REFRESH_TOKEN_COOKIE_NAME: str = getenv("REFRESH_TOKEN_COOKIE_NAME", "refresh_token")
+COOKIE_SECURE: bool = getenv("COOKIE_SECURE", "false").lower() == "true"
+COOKIE_SAMESITE: str = getenv("COOKIE_SAMESITE", "lax")
